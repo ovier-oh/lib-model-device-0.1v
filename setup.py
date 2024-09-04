86c252ac53abdda09f7dac083984fa945dd186f0
ovier-oh <ovier.obregon.hin@outlook.com>
2024-09-04 23:32:01 +0300

    actualizando setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-# Archivo: setup.py 
-from setuptools import setup, find_packages 
+# Archivo: setup.py
+
+from setuptools import setup, find_packages
 
 setup(
-        name = 'model_device',      #Nombre del paquete 
-        version = '0.1.0',          # Version dle paquete 
-        packages = find_packages()  # Encuentra automaticamente los paquetes en la carpeta 
-        install_requieres = [
-            "numpy",
-            "matplotlib",
-            ],
-        author = "Ovier ObregonH", 
-        description = "Libreria para modelar fisicamente dispositivoss electronicos", 
-        long_description=open("README.md").read()
-        long_description_content_type = "text/markdown", 
-        url="https://github.com/ovier-oh/lib-model-device-0.1v",
-        classifiers = [
-            "Programming Language :: Python :: 3",
-            "License :: OSI Approved :: MIT License",
-            "Operating System :: OS Independent",
-            ],
-            python_requires='>=3.6', #Version minima de python
-        )
+    name="model_device",  # Nombre del paquete
+    version="0.1.0",      # Versión del paquete
+    packages=find_packages(),  # Encuentra automáticamente los paquetes en la carpeta
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],  # Dependencias necesarias
+    author="Tu Nombre",
+    description="Librería para modelar dispositivos electrónicos",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tuusuario/proyecto_modelo_diodo",  # Cambia esto si subes a GitHub
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Versión mínima de Python requerida
+)
